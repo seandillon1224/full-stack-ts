4ff713f269678fad050663d63497481a77b6aa27
Sean Dillon <seandillon1224@gmail.com>
2022-08-12 12:57:42 -0400

    Initial schema and server setup

---
+++ server/src/apollo-server.ts
@@ -0,0 +1,33 @@
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import { ApolloServer, ExpressContext, gql } from 'apollo-server-express';
+import * as express from 'express';
+import { Server } from 'http';
+import Db from './db';
+// Import Schema Modules
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { GRAPHQL_SCHEMA_PATH } from './constants';
+import resolvers from './resolvers';
+
+const SCHEMA = loadSchemaSync(GRAPHQL_SCHEMA_PATH, {
+  loaders: [new GraphQLFileLoader()],
+});
+
+export async function createApolloServer(
+  db: Db,
+  httpServer: Server,
+  app: express.Application
+): Promise<ApolloServer<ExpressContext>> {
+  const server = new ApolloServer({
+    schema: addResolversToSchema({
+      schema: SCHEMA,
+      resolvers,
+    }),
+    context: () => ({ db }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  return server;
+}
---
+++ server/src/resolvers/Query.ts
@@ -0,0 +1,20 @@
+const queryTwitterResolver = {
+  Query: {
+    currentUser: () => {
+      return {
+        id: '123',
+        name: 'John Doe',
+        handle: 'johndoe',
+        coverUrl: '',
+        avatarUrl: '',
+        createdAt: '',
+        updatedAt: '',
+      };
+    },
+    suggestions: () => {
+      return [];
+    },
+  },
+};
+
+export default queryTwitterResolver;
